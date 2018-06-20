194425b6f75554c47779c652d0947c1ee096eba4
mohammad <shalabi67@hotmail.com>
2018-06-20 21:52:33 +0200

    adding netwroking

---
+++ ShellScripting/functions/function1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#shows how to use functions
+
+func()
+{
+    echo "you called func"
+}
+
+#call your functions
+func
+func
+func
---
+++ ShellScripting/menu/menu2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#shows how to show menu
+
+options="option1 option2 option3 option4"
+PS3="Enter your choice: "  #set menu prompt message
+select choice in $options ; do
+    echo "Reply variable is $REPLY"
+    echo "choice is $choice"
+
+    case $REPLY in
+    "1")
+    echo "selected option 1"
+    break
+    ;;
+    "2")
+    echo "selected option 2"
+    break
+    ;;
+    "3")
+    echo "selected option 3"
+    break
+    ;;
+    "4")
+    echo "selected option 4"
+    break
+    ;;
+    *)
+    echo "Invalid selection."
+    ;;
+
+    esac
+done
